2beca60e19f1f2afc42e1534304c9101459a9dbf
Flippy Sulistyo <bayusulistyo16@gmail.com>
2019-08-17 18:47:08 +0700

    NewAgain

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = new Vue({
     methods: {
         addTodo(){
             if(this.todo.length > 0){
+                this.warnText= '';
                 this.arrTodo.unshift({
                     title: this.todo,
                     isDone: false
